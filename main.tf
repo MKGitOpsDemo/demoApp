7d1ca51a5da51106d6f726bcf1621bfdf92ebb6c
Mike <mike.kee@gmail.com>
2020-05-31 21:54:46 +0100

    patching in ubuntu

--- main.tf
+++ main.tf
@@ -19,39 +19,40 @@ output "_ci" {
   value = "ci"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-disco-19.04-amd64-server-*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-disco-19.04-amd64-server-*"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
 
-  owners = ["099720109477"] # Canonical
-}
+#   owners = ["099720109477"] # Canonical
+# }
 
-resource "aws_instance" "mgmthost" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-#   key_name               = "devops_admin_key2"
-#   vpc_security_group_ids = [aws_security_group.mgmt_node.id]
+# resource "aws_instance" "mgmthost" {
+#   ami                    = data.aws_ami.ubuntu.id
+#   instance_type          = "t2.micro"
+# #   key_name               = "devops_admin_key2"
+# #   vpc_security_group_ids = [aws_security_group.mgmt_node.id]
 
-  user_data = <<-EOF
-        #!/bin/bash
-        sudo apt update
-        sudo apt-get -y install awscli
+#   user_data = <<-EOF
+#         #!/bin/bash
+#         sudo apt update
+#         sudo apt-get -y install awscli
 
-        sudo apt-get -y install software-properties-common
-        sudo apt-add-repository --yes --update ppa:ansible/ansible
-        sudo apt-get -y install ansible
-        EOF
+#         sudo apt-get -y install software-properties-common
+#         sudo apt-add-repository --yes --update ppa:ansible/ansible
+#         sudo apt-get -y install ansible
+#         EOF
 
-  tags = {
-    "role" : "MgmtHost"
-  }
-}
+#   tags = {
+#     "role" : "MgmtHost"
+#   }
+# }
+
